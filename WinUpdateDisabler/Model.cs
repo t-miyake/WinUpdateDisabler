950376f92f8bb659e040ca0a5adc9bdc732f3111
T.Miyake <t@miyake.me>
2018-10-25 11:23:12 +0900

    Initial commit.

---
+++ WinUpdateDisabler/Model.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Win32;
+
+namespace WinUpdateDisabler
+{
+    public sealed class Model
+    {
+        // Singleton instance.
+        internal static Model Instance { get; } = new Model();
+        private Model()
+        {
+
+        }
+
+        internal bool CurrentState { get; set; } = true;
+
+        internal void GetCurrentStatus()
+        {
+            switch (ReadReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "WUServer"))
+            {
+                case null:
+                    CurrentState = true;
+                    break;
+                case "http://localhost/":
+                    CurrentState = false;
+                    break;
+                default:
+                    CurrentState = true;
+                    break;
+            }
+        }
+
+        internal void DoDisable()
+        {
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU", "NoAutoRebootWithLoggedOnUsers", "1", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU", "NoAutoUpdate", "1", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU", "UseWUServer", "1", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DoNotConnectToWindowsUpdateInternetLocations", "1", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DisableDualScan", "1", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "BranchReadinessLevel", "20", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DeferFeatureUpdates", "0", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "DeferFeatureUpdatesPeriodInDays", "0", RegistryValueKind.DWord);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "PauseFeatureUpdatesStartTime", "",RegistryValueKind.String);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "UpdateServiceUrlAlternate", @"http://localhost/", RegistryValueKind.String);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "WUServer", @"http://localhost/", RegistryValueKind.String);
+            WriteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate", "WUStatusServer", @"http://localhost/", RegistryValueKind.String);
+        }
+
+        internal void DoEnable()
+        {
+            DeleteReg(@"SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate");
+        }
+
+        private void WriteReg(string subKey, string keyName, string value, RegistryValueKind kind)
+        {
+            var key = Registry.LocalMachine.CreateSubKey(subKey);
+            key?.SetValue(keyName, value, kind);
+            key?.Close();
+        }
+
+        private void DeleteReg(string subKey)
+        {
+            try
+            {
+                Registry.LocalMachine.DeleteSubKeyTree(subKey);
+            }
+            catch
+            {
+                // Do Noting.
+            }
+        }
+
+        private string ReadReg(string subKey, string keyName)
+        {
+            try
+            {
+                var key = Registry.LocalMachine.OpenSubKey(subKey);
+                return key?.GetValue(keyName).ToString();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ WinUpdateDisabler/ViewModel.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection.Emit;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WinUpdateDisabler
+{
+    public class ViewModel : ViewModelBase
+    {
+        private readonly Model _model = Model.Instance;
+
+        public ICommand Enable { get; }
+        public ICommand Disable { get; }
+
+        public ViewModel()
+        {
+            Enable = new RelayCommand(() =>
+            {
+                _model.DoEnable();
+                CurrentState = true;
+                UpdateButton();
+
+                if (MessageBox.Show("Enabled" + Environment.NewLine + "Please reboot.", "WinUpdateDisabler", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                    Reboot();
+            });
+
+            Disable = new RelayCommand(() =>
+            {
+                _model.DoDisable();
+                CurrentState = false;
+                UpdateButton();
+
+                if (MessageBox.Show("Disabled" + Environment.NewLine + "Please sign out.", "WinUpdateDisabler", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                    SignOut();
+            });
+
+            _model.GetCurrentStatus();
+            UpdateButton();
+        }
+
+        public bool CurrentState
+        {
+            get => _model.CurrentState;
+            set
+            {
+                _model.CurrentState = value;
+                OnPropertyChanged("CurrentStateText");
+            }
+        }
+
+        public string CurrentStateText => _model.CurrentState ? "Current state：Enabled" : "Current state：Disabled";
+
+        private bool _enableButton;
+        public bool EnableButton
+        {
+            get => _enableButton;
+            set
+            {
+                _enableButton = value;
+                OnPropertyChanged("EnableButton");
+            }
+        }
+
+        private bool _disableButton;
+        public bool DisableButton
+        {
+            get => _disableButton;
+            set
+            {
+                _disableButton = value;
+                OnPropertyChanged("DisableButton");
+            }
+        }
+
+        private void UpdateButton()
+        {
+            if (CurrentState)
+            {
+                EnableButton = false;
+                DisableButton = true;
+            }
+            else
+            {
+                EnableButton = true;
+                DisableButton = false;
+            }
+        }
+
+        private static void SignOut()
+        {
+            var signOut = new ProcessStartInfo
+            {
+                FileName = "shutdown.exe",
+                Arguments = "/l",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process.Start(signOut);
+        }
+
+        private static void Reboot()
+        {
+            var signOut = new ProcessStartInfo
+            {
+                FileName = "shutdown.exe",
+                Arguments = "/r /t 0",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process.Start(signOut);
+        }
+    }
+}
